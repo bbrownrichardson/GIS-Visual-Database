f4453942cfebc24f217c5acffa371d33ab6b5fed
Brianna Brown Richardson <bbrownrichardson19@wooster.edu>
2018-04-18 13:48:39 -0700

    Interface.py create db file
    If database file is not present, a file will be automatically created

--- interface.py
+++ interface.py
@@ -5,28 +5,92 @@
 
 from read_shapefile import ReadShapeFile
 from sql_functions import InsertDatabase, IndividualGetDatabase, GetAllDatabase
+from error_handling import Invalid
 from upload_requirements import UploadRequirements
 from visual_setup import VisualSetup
 import os
 import app_screens
+import sqlite3
 
 
 class Interface:
     def __init__(self):
-        self._db_file = 'db.sqlite'
         self.title = None
+        self._main_dir = None
+        self.selected_files_dir = list()
+        self.selected_files = list()
+        self.if_not_exist()
+        self._db_file = 'db.sqlite'
+
+    def if_not_exist(self):
         if not os.path.exists(os.getcwd() + '\Uploaded_Shapefiles' + chr(92)):
             os.makedirs(os.getcwd() + '\Uploaded_Shapefiles' + chr(92))
         self._main_dir = os.getcwd() + '\Uploaded_Shapefiles' + chr(92)
-        self.selected_files_dir = []
-        self.selected_files = []
+        if not os.path.isfile(os.getcwd() + '\db.sqlite'):
+            self.create_tables()
+
+    def create_tables(self):
+        conn = sqlite3.connect('db.sqlite')
+        cur = conn.cursor()
+
+        cur.execute('DROP TABLE IF EXISTS Profile')
+        cur.execute('DROP TABLE IF EXISTS Object')
+        cur.execute('DROP TABLE IF EXISTS Location')
+        cur.execute('DROP TABLE IF EXISTS Files')
+
+        cur.execute(
+            'CREATE TABLE Profile('
+            'ProfileId INTEGER NOT NULL,'
+            'Creator NVARCHAR(160) unique NOT NULL,'
+            'CONSTRAINT PK_Profile PRIMARY KEY(ProfileId))')
+
+        cur.execute(
+            'CREATE TABLE Object('
+            'ObjectId INTEGER NOT NULL,'
+            'Title NVARCHAR(160) UNIQUE NOT NULL,'
+            'Date DATETIME,'
+            'ProfileId INTEGER NOT NULL,'
+            'CONSTRAINT PK_Object PRIMARY KEY(ObjectId),'
+            'FOREIGN KEY (ProfileId) REFERENCES Profile(ProfileId)'
+            '   ON DELETE CASCADE )')
+
+        cur.execute(
+            'CREATE TABLE Location('
+            'LocationId INTEGER  NOT NULL,'
+            'Address NVARCHAR(160),'
+            'City NVARCHAR(160),'
+            'State NVARCHAR(160),'
+            'Country NVARCHAR(160) NOT NULL,'
+            'Zipcode INTEGER,'
+            'ObjectId INTEGER NOT NULL,'
+            'CONSTRAINT PK_Location PRIMARY KEY  (LocationId),'
+            'FOREIGN KEY (ObjectId) REFERENCES Object(ObjectId)'
+            'ON DELETE CASCADE )')
+
+        cur.execute(
+            'CREATE TABLE Files('
+            'FilesId INTEGER  NOT NULL,'
+            'ShpName NVARCHAR(160) NOT NULL,'
+            'DbfName NVARCHAR(160) NOT NULL,'
+            'FileDirectory NVARCHAR(160) NOT NULL,'
+            'ObjectId INTEGER NOT NULL,'
+            'CONSTRAINT PK_Files PRIMARY KEY(FilesId),'
+            'FOREIGN KEY (ObjectId) REFERENCES Object(ObjectId)ON '
+            '   DELETE CASCADE)')
+
+        cur.execute('CREATE INDEX IFK_ProfileId ON Profile(ProfileId)')
+        cur.execute('CREATE INDEX IFK_ObjectId ON Object(ObjectId)')
+        cur.execute('CREATE INDEX IFK_LocationId ON Location(LocationId)')
+        cur.execute('CREATE INDEX IFK_FilesId ON Files(FilesId)')
+
+        conn.commit()
+        self._db_file = 'db.sqlite'
 
     def sql_to_object_screen(self):
         """
         Retrieve all profiles present in database
         :return: all SQL database data
         """
-
         method_1 = GetAllDatabase(self._db_file)
         all_obj_list = method_1.get_all()
         return all_obj_list
@@ -37,17 +101,20 @@ class Interface:
         :param title: unique title of the profile being selected
         :return: None
         """
+        try:
+            obj = IndividualGetDatabase(title, self._db_file)
+            obj.get_object()
+            temp = obj.get_files()
+            r_obj = ReadShapeFile(self._main_dir, temp['ShpName'],
+                                  temp['DbfName'], title)
+            a = r_obj.read_files(temp['FileDirectory']+temp['ShpName'],
+                                 temp['FileDirectory']+temp['DbfName'])
+            v_obj = VisualSetup(a)
+            app_screens.VisualScreen.plt_var = None
+            app_screens.VisualScreen.plt_var = v_obj.get_plt_2d()
 
-        obj = IndividualGetDatabase(title, self._db_file)
-        obj.get_object()
-        temp = obj.get_files()
-        r_obj = ReadShapeFile(self._main_dir, temp['ShpName'],
-                              temp['DbfName'], title)
-        a = r_obj.read_files(temp['FileDirectory'] + temp['ShpName'],
-                             temp['FileDirectory'] + temp['DbfName'])
-        v_obj = VisualSetup(a)
-        app_screens.VisualScreen.plt_var = None
-        app_screens.VisualScreen.plt_var = v_obj.get_plt_2d()
+        except Invalid as e:
+            app_screens.PopupError(e)
 
     def shape_file_three_dimension(self, title):
         """
@@ -151,3 +218,11 @@ class Interface:
                             self.selected_files[0]['filename'] +
                             self.selected_files[0]['extension'],
                             new_dir)
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
